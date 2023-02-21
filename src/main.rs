dc48d06c42f76bb1771af1b333f38791fa81154e
Tim Newsome <tim@casualhacker.net>
2023-02-20 19:02:53 -0800

    Correctly track at least letters that are definitely present
    I think/hope.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use clap::Parser;
+use std::cmp;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -20,23 +21,93 @@ struct CharacterConstraint {
 
 #[derive(Debug)]
 struct Constraint {
-    character: Vec<CharacterConstraint>
+    character: Vec<CharacterConstraint>,
+    // For each char, track how many there are at least in the word.
+    min_occurrence: HashMap<char, usize>
 }
 
 impl Constraint {
     pub fn new(size: usize) -> Self {
         let character = vec![CharacterConstraint {is: None, is_not: HashSet::new() }; size];
-        Self { character }
+        Self {
+            character,
+            min_occurrence: HashMap::new()
+        }
+    }
+
+    pub fn from_string(string: &String, size: usize) -> Self {
+        let mut constraint = Constraint::new(size);
+        let mut i = 0;
+        enum Op {
+            Green,
+            Yellow,
+            Gray
+        }
+        let mut op = Op::Green;
+        for c in string.chars() {
+            match c {
+                ' ' => { i += 1; op = Op::Green; },
+                '-' => op = Op::Gray,
+                '~' => op = Op::Yellow,
+                x => {
+                    match op {
+                        Op::Green => {
+                            constraint.character[i].is = Some(x);
+                            constraint.increment_min_occurrence(&x)
+                        },
+                        Op::Yellow => {
+                            constraint.character[i].is_not.insert(x);
+                            constraint.increment_min_occurrence(&x)
+                        },
+                        Op::Gray => {
+                            constraint.character[i].is_not.insert(x);
+                            ()
+                        }
+                    }
+                }
+            }
+        }
+        constraint
     }
 
     pub fn score(self) -> usize {
-        let mut s = 0;
-        for c in self.character {
-            s += if c.is != None { 10 } else { 0 };
-            s += c.is_not.len();
+        let mut s = self.min_occurrence.values().sum();
+        for cc in self.character {
+            s += if cc.is != None { 10 } else { 0 };
+            s += cc.is_not.len();
         }
         s
     }
+
+    pub fn increment_min_occurrence(&mut self, c: &char) {
+        self.min_occurrence.entry(*c).and_modify(|n| *n += 1).or_insert(1);
+    }
+
+    pub fn update(&mut self, constraint: &Constraint) {
+        for (c, count) in constraint.min_occurrence.iter() {
+            self.min_occurrence.entry(*c)
+                    .and_modify(|v| *v = cmp::max(*v, *count))
+                    .or_insert(*count);
+        }
+        for (my_c, other_c) in self.character.iter_mut().zip(constraint.character.iter()) {
+            if other_c.is == None {
+                for c in other_c.is_not.iter() {
+                    my_c.is_not.insert(*c);
+                }
+            } else {
+                my_c.is = other_c.is;
+            }
+        }
+    }
+}
+
+fn char_frequency(word: &str) -> HashMap<char, usize>
+{
+    let mut char_frequency = HashMap::new();
+    for c in word.chars() {
+        char_frequency.entry(c).and_modify(|n| *n += 1).or_insert(1);
+    }
+    char_frequency
 }
 
 fn wordle_guess(guess: &str, answer: &str) -> Constraint
@@ -47,8 +118,21 @@ fn wordle_guess(guess: &str, answer: &str) -> Constraint
             constraint.character[i].is = Some(g);
         } else {
             constraint.character[i].is_not.insert(g);
+            // TODO: increment_min_occurrence() if this character exists elsewhere in the word
         }
     }
+
+    let guess_frequency = char_frequency(guess);
+    let answer_frequency = char_frequency(answer);
+
+    for (c, guess_count) in guess_frequency.iter() {
+        let answer_count = answer_frequency.get(c).unwrap_or(&0);
+        let constraint_count = cmp::min(guess_count, answer_count);
+        if constraint_count > &0 {
+            constraint.min_occurrence.insert(*c, *constraint_count);
+        }
+    }
+
     constraint
 }
 
@@ -57,9 +141,10 @@ fn filter_words(constraint: Constraint, words: &Vec<String>) -> Vec<&String>
     let mut v = Vec::new();
 
     for word in words {
+        let char_frequency = char_frequency(word.as_str());
         if
-            // Word must not contain letters that aren't present.
-            // !constraint.does_not_contain.iter().any(|&x| word.contains(x)) &&
+            constraint.min_occurrence.iter()
+                    .all(|(key, value)| char_frequency.get(key).unwrap_or(&0) >= value) &&
             // Check that green letters are where they should be.
             constraint.character.iter().zip(word.chars())
                     .all(|(cc, y)| 
@@ -115,35 +200,16 @@ fn main()
 
     let words = read_words(&cli.words.unwrap_or("words".to_string()), word_length);
 
-    let mut constraint = Constraint::new(word_length);
+    let mut constraint_acc = Constraint::new(word_length);
     for constraint_string in cli.information {
-        let mut i = 0;
-        enum Op {
-            Is,
-            IsNot,
-            Exclude
-        }
-        let mut op = Op::Is;
-        for c in constraint_string.chars() {
-            match c {
-                ' ' => { i += 1; op = Op::Is; },
-                '-' => op = Op::Exclude,
-                '~' => op = Op::IsNot,
-                x => {
-                    match op {
-                        Op::Is => constraint.character[i].is = Some(x),
-                        Op::IsNot | Op::Exclude => {constraint.character[i].is_not.insert(x); ()},
-                        //Op::Exclude => {constraint.does_not_contain.insert(x); ()}
-                    }
-                }
-            }
-        }
+        let constraint = Constraint::from_string(&constraint_string, word_length);
         println!("Constraint: {:?}", constraint);
+        constraint_acc.update(&constraint);
     }
 
-    let remaining_words = filter_words(constraint, &words);
+    let remaining_words = filter_words(constraint_acc, &words);
     println!("{}/{} words remaining", remaining_words.len(), words.len());
-    if remaining_words.len() < 10 {
+    if remaining_words.len() < 15 {
         for w in &remaining_words {
             println!("  {}", w)
         }
